16a722b677265863f592fac8dcb9dd456139a5b5
Ryan Wiebe <wieberockin@yahoo.com>
2018-08-25 18:27:00 -0600

    Initial Code Commit

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,8 +10,29 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            /*
+             int x;
+             int y;
+             x = 7;
+             y = x + 3;
+             Console.WriteLine(y);
+             Console.Readline()
+             */
+
+            Console.WriteLine("What is Your name?");
+            Console.Write("Type your first name: ");
+            string myFirstName = Console.ReadLine();
+
+            //string myLastName;
+            //Console.Write("Type your last name: ");
+            //myLastName = Console.ReadLine();
+
+            Console.WriteLine("Type your last name: ");
+            string myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+            
         }
     }
 }
